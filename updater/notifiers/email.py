62d96a29c294b9005b59882f8688fcd78f64c0b7
Rob Brackett <rob@robbrackett.com>
2012-08-15 16:28:45 -0700

    Set all the text properly in email templates, allow templates to also specify subject line. Fixes #2 and #12.

--- updater/notifiers/email.py
+++ updater/notifiers/email.py
@@ -2,6 +2,9 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from jinja2 import Environment, FileSystemLoader
+from dateutil.parser import parse as parse_date
+
+SUBJECT_KEY = 'Subject:'
 
 # Notifier interface
 NOTIFICATION_METHOD = 'email'
@@ -26,23 +29,54 @@ def send_notifications(notifications, options):
 
 
 def send_email_notification(address, sr, smtp, options, template_env):
+    # parse dates in SR (in case the template wants to display them)
+    # could potentially do this in the core updater instead of the mail plugin
+    sr['requested_datetime'] = parse_date(sr['requested_datetime'])
+    sr['updated_datetime'] = parse_date(sr['updated_datetime'])
+    if 'notes' in sr:
+        for note in sr['notes']:
+            note['datetime'] = parse_date(note['datetime'])
+    
+    # basic stuff needed for sending and templates
     from_address = options.EMAIL_FROM or options.EMAIL_USER
+    default_subject = 'Chicago 311: Your %s issue has been %s' % (sr['service_name'], sr['status'] == 'open' and 'updated.' or 'completed!')
     details_url = options.SR_DETAILS_URL.format(sr_id=sr['service_request_id'])
     img_path = options.SR_TRACKER_IMG
     
-    subject = 'Chicago 311: Your %s issue has been updated.' % sr['service_name']
-    
+    # render message template
     html_template = template_env.get_template('email.html')
-    html_body = html_template.render(sr=sr, details_url=details_url, img=img_path)
     text_template = template_env.get_template('email.txt')
-    text_body = text_template.render(sr=sr, details_url=details_url, img=img_path)
+    html_body = html_template.render(sr=sr, details_url=details_url, img=img_path, subject=default_subject)
+    text_body = text_template.render(sr=sr, details_url=details_url, img=img_path, subject=default_subject)
     
+    # get subject
+    html_subject, html_body = subject_from_message(html_body)
+    text_subject, text_body = subject_from_message(text_body)
+    subject = html_subject or text_subject or default_subject
+    
+    # create the actual message
     message = MIMEMultipart('alternative')
     message['Subject'] = subject
     message['From'] = from_address
     message['To'] = address
     message.attach(MIMEText(text_body, 'plain'))
     message.attach(MIMEText(html_body, 'html'))
-
-    smtp.sendmail(from_address, [address], message.as_string())
     
+    # and send!
+    smtp.sendmail(from_address, [address], message.as_string())
+
+
+def subject_from_message(text):
+    '''
+    If the is in the format: "Subject: [subject]\n\n[content]"
+    then pull off the first two lines and return a tuple of subject and content.
+    If not in the above format, return null for subject and the original string for content.
+    '''
+    parts = text.split('\n', 2)
+    subject = None
+    body = text
+    if len(parts) > 2 and parts[0].startswith(SUBJECT_KEY) and parts[1] == '':
+        subject = parts[0][len(SUBJECT_KEY):].strip()
+        body = parts[2]
+        
+    return (subject, body)
