34c7a16d266e44b53f302393b6945dbe0845f887
Rob Brackett <rob@robbrackett.com>
2012-09-18 17:43:16 -0700

    Adding license info to new source files

--- static/js/create_service_request.js
+++ static/js/create_service_request.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) 2012, Code for America
+ * This is open source software, released under a standard 3-clause
+ * BSD-style license; see the file LICENSE for details.
+ */
+
 $(document).ready(function() {
 
 	var DEFAULT_POSITION = {
