757170b2b08df0671b512ffe6b99b7f8b5d9fe0d
Rob Brackett <rob@robbrackett.com>
2012-12-03 10:44:10 -0800

    Remove unnecessary debug statement

--- app.py
+++ app.py
@@ -62,8 +62,6 @@ def index():
     if app.config['OPEN311_API_KEY']:
         params['api_key'] = app.config['OPEN311_API_KEY']
 
-    app.logger.debug('RECENT SRs: %s', params)
-
     r = requests.get(url, params=params)
     if r.status_code != 200:
         app.logger.error('OPEN311: Failed to load recent requests from Open311 server. Status Code: %s, Response: %s', r.status_code, r.text)
